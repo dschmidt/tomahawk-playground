f40dbeb3af2b29590711eb4d85283f0dafdd2841
Dominik Schmidt <dev@dominik-schmidt.de>
2012-04-21 00:45:30 +0200

    referencing back works as well

--- databasecommand_addfiles.cpp
+++ databasecommand_addfiles.cpp
@@ -1,9 +1,11 @@
 #include "databasecommand_addfiles.h"
 
+#include "libtomahawk.h"
 
 DatabaseCommand_Addfiles::DatabaseCommand_Addfiles()
 {
-
+    // circle .. better don't lunch this program :P
+    new libtomahawk();
 }
 
 
